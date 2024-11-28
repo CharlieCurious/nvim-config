18b4465ba23ef432eb95d4d22d786a9f3ea87013
CharlieCurious <dfrco2@outlook.com>
2024-11-28 23:57:28 +0200

    Added grep to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,3 +2,4 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git tracked files' })
 vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }) -- requires ripgrep: https://github.com/BurntSushi/ripgrep.git
