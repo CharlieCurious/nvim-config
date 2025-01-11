fe2ff1d606812b7a2679a159bfd6ab02429d0dd8
CharlieCurious <dfrco2@outlook.com>
2025-01-11 11:08:45 +0200

    Remove leading spaces and white lines at the bottom

--- lua/charlie/init.lua
+++ lua/charlie/init.lua
@@ -1,3 +1,16 @@
 require("charlie.remap")
 require("charlie.set")
 require("charlie.lazy_init")
+
+-- Function to trim trailing whitespace and empty lines
+local function trim_trailing_spaces_and_lines()
+  -- Remove trailing spaces
+  vim.cmd([[ %s/\s\+$//e ]])
+  -- Remove empty lines at the end of the file
+  vim.cmd([[ %s/\n\+\%$//e ]])
+end
+
+-- Automatically trim on buffer write
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = trim_trailing_spaces_and_lines,
+})
